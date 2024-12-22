b05580c7e7ab77cbee429f6ecb7daa6bd9aef269
DEVFBA <angenrique.gutierrez@gmail.com>
2024-12-21 19:53:07 -0600

    Fix: Return error of Duplicated Email on Post User Response

--- src/routes/users-routes.js
+++ src/routes/users-routes.js
@@ -52,8 +52,18 @@ router.post('/', async (req, res) => {
     user = await createUser(user)
     res.status(201).send({ status: 'OK', data: user, error: null })
   } catch (error) {
-    console.log(error)
-    res.status(400).send({ status: 'Error', data: null, error: error })
+    console.log('Error ==>', error.errors?.Birth_Date.properties.type)
+
+    let errorInfo = error
+
+    if (error.errorResponse?.code === 11000) {
+      errorInfo = {
+        code: error.errorResponse.code,
+        message: 'Email already exists'
+      }
+    }
+
+    res.status(400).send({ status: 'Error', data: null, error: errorInfo })
   }
 })
 
@@ -85,7 +95,7 @@ router.put('/:id', validUser, async (req, res) => {
       res.status(200).send({ status: 'OK', data: updatedUser, error: null })
     } else {
       res
-        .status(401)
+        .status(403)
         .send({ message: 'Unauthorized User', data: null, error: null })
     }
   } catch (error) {
--- src/useCases/users-useCases.js
+++ src/useCases/users-useCases.js
@@ -8,6 +8,7 @@ const createUser = async (user) => {
     return newUser
   } catch (error) {
     console.log(error)
+    throw error
   }
 }
 
